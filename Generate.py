19356af987261cc9608284cc45f7c96eae839938
ba50 <barjas02@gmail.com>
2019-10-16 22:35:10 +0200

    End of generating structure

--- Generate.py
+++ Generate.py
@@ -8,29 +8,32 @@ import pandas as pd
 from src.GenerateXYZ import GenerateXYZ
 
 
-def generate_sim_input(params_dict, structure: GenerateXYZ):
-    (params_dict['path_to_data'] / 'heat_map').mkdir(parents=True, exist_ok=True)
+def generate_sim_input(_row, _path_to_data, _structure: GenerateXYZ):
+    (_path_to_data / 'heat_map').mkdir(parents=True, exist_ok=True)
 
-    with (params_dict['path_to_data'] / 'input.kmc').open('w') as file_out:
-        file_out.write("{}\t# cell_type\n".format(params_dict['cell_type'].lower()))
-        file_out.write("{}\t# size_x\n".format(params_dict['size'][0]))
-        file_out.write("{}\t# size_y\n".format(params_dict['size'][1]))
-        file_out.write("{}\t# size_z\n".format(params_dict['size'][2]))
-        file_out.write("{}\t# therm_time\n".format(params_dict['thermalization_time']))
-        file_out.write("{}\t# sim_time\n".format(params_dict['time_end']))
-        file_out.write("{}\t# window\n".format(params_dict['window']))
-        file_out.write("{}\t# window_epsilon\n".format(params_dict['window_epsilon']))
-        file_out.write("{}\t# left_contact_sw\n".format(params_dict['contact_switch'][0]))
-        file_out.write("{}\t# right_contact_sw\n".format(params_dict['contact_switch'][1]))
-        file_out.write("{}\t# left_contact\n".format(params_dict['contact'][0]))
-        file_out.write("{}\t# right_contact\n".format(params_dict['contact'][1]))
-        file_out.write("{}\t# amplitude\n".format(params_dict['energy_params']['amplitude']))
-        file_out.write("{}\t# frequency_base\n".format(params_dict['energy_params']['frequency_base']))
-        file_out.write("{}\t# frequency_power\n".format(params_dict['energy_params']['frequency_power']))
-        file_out.write("{}\t# period\n".format(params_dict['energy_params']['periods']))
-        file_out.write("{}\t# energy_base\n".format(params_dict['energy_params']['energy_base']))
+    with (_path_to_data / 'input.kmc').open('w') as file_out:
+        file_out.write("{}\t# cell_type\n".format(_row['cell_type'].lower()))
+        file_out.write("{}\t# size_x\n".format(_row['size_x']))
+        file_out.write("{}\t# size_y\n".format(_row['size_y']))
+        file_out.write("{}\t# size_z\n".format(_row['size_z']))
+        file_out.write("{}\t# therm_time\n".format(_row['thermalization_time']))
+        file_out.write("{}\t# sim_time\n".format(_row['time_end']))
+        file_out.write("{}\t# window\n".format(_row['window']))
+        file_out.write("{}\t# window_epsilon\n".format(_row['window_epsilon']))
+        file_out.write("{}\t# left_contact_sw\n".format(_row['contact_switch_left']))
+        file_out.write("{}\t# right_contact_sw\n".format(_row['contact_switch_right']))
+        file_out.write("{}\t# left_contact\n".format(_row['contact_left']))
+        file_out.write("{}\t# right_contact\n".format(_row['contact_right']))
+        file_out.write("{}\t# amplitude\n".format(_row['amplitude']))
+        file_out.write("{}\t# frequency\n".format(_row['frequency']))
+        file_out.write("{}\t# periods\n".format(_row['periods']))
+        file_out.write("{}\t# energy_base\n".format(_row['energy_base']))
 
-        structure.save_positions(params_dict['path_to_data']/'positions.xyz')
+        _structure.save_positions(_path_to_data/'positions.xyz')
+
+
+def exp_decay(x, amp=50, tau=5):
+    return (amp * np.exp(-x / tau)).astype(int)
 
 
 if __name__ == '__main__':
@@ -49,61 +52,54 @@ if __name__ == '__main__':
     simulations = pd.DataFrame({'frequency': freq_list})
 
     simulations['version'] = 'a'
-    simulations['cell_type'] = 'Random'
+    simulations['cell_type'] = 'random'
     simulations['size_x'] = 30
     simulations['size_y'] = 7
     simulations['size_z'] = 7
     simulations['time_end'] = 0
     simulations['thermalization_time'] = 200
-    simulations['window'] = 10
+    simulations['window'] = 100
     simulations['window_epsilon'] = 0.01
     simulations['contact_switch_left'] = 0
     simulations['contact_switch_right'] = 0
     simulations['contact_left'] = 1
     simulations['contact_right'] = 1
+    simulations['amplitude'] = 0.008
+    simulations['energy_base'] = 0
 
-    simulations = simulations.loc[np.repeat(simulations.index.values, 3)].reset_index(drop=True)
-    simulations['version'] = np.array([[x for x in ['a', 'b', 'c']] for _ in range(len(freq_list))]).flatten()
-    simulations['size_x'] = simulations.index.map(lambda x: (30 * np.exp(-x/30)).astype(int))
-    print()
-    exit()
+    simulations['periods'] = simulations['frequency'].map(
+        lambda freq: np.clip(freq / freq_list[0] * base_periods, 0, 10)
+    )
 
-    params = {}
-    sym_path_list = []
-    sim_structure = GenerateXYZ(params_base['size'])
-    sim_structure.generate_random()
-    for index, freq in enumerate(freq_list):
-        for s in repeat_list:
-            params_per_sim = copy(params_base)
-            temp_dict = {}
-            sym_name = '_'.join([str(params_base['size'][0]),
-                                 str(params_base['size'][1]),
-                                 str(params_base['size'][2]),
-                                 params_base['cell_type'].lower(),
-                                 str(index),
-                                 s])
-            temp_dict['path_to_data'] = save_path / sym_name
-            sym_path_list.append(temp_dict['path_to_data'])
+    version = ['a', 'b', 'c']
 
-            temp_dict['energy_params'] = {'amplitude': 0.008,
-                                          'frequency': freq,
-                                          'periods': freq/freq_list[0]*base_periods,
-                                          'energy_base': 0}
-            params_per_sim.update(temp_dict)
-            generate_sim_input(params_per_sim, sim_structure)
+    simulations = simulations.loc[np.repeat(simulations.index.values, 3)].reset_index()
+    simulations['version'] = np.array([[x for x in version] for _ in range(len(freq_list))]).flatten()
 
-    run_df = {'commend': []}
-    for s in repeat_list:
-        for sim_path in sym_path_list:
-            if sim_path.stem[-1] == s:
-                run_df['commend'].append(str(bin_path)+' '+str(sim_path))
-
-    run_df = pd.DataFrame(run_df)
-    run_df['commend'] = run_df['commend'].map(lambda x: x+'\n')
-    for index, split in enumerate(np.split(run_df, workers)):
-        if os.name == 'nt':
-            with Path(save_path, 'run_%s.ps1' % index).open('w') as f_out:
-                f_out.writelines(split['commend'])
+    size_x = []
+    for x in range(len(freq_list)):
+        y = exp_decay(x)
+        if (exp_decay(x) % 2) == 0:
+            size_x.append(y - 1)
         else:
-            with Path(save_path, 'run_%s.run' % index).open('w') as f_out:
-                f_out.writelines(split['commend'])
+            size_x.append(y)
+
+    simulations['size_x'] = np.flip(np.repeat(np.clip(np.array(size_x), 13, 30), len(version)))
+    simulations['sim_name'] = simulations.apply(
+        lambda x: '_'.join([str(x[4]), str(x[5]), str(x[6]), x[3], str(x[0]), x[2]]), axis=1
+    )
+
+    simulations['path_to_data'] = simulations['sim_name'].map(lambda x: save_path / x)
+    simulations['commend'] = simulations['path_to_data'].map(lambda x: str(bin_path)+' '+str(x)+'\n')
+    for index, chunk in enumerate(np.split(simulations, workers)):
+        for _, row in chunk.iterrows():
+            path_to_data = save_path / row['sim_name']
+            sim_structure = GenerateXYZ((row['size_x'], row['size_y'], row['size_z']))
+            generate_sim_input(row, path_to_data, sim_structure)
+            commend = str(bin_path)+' '+str(path_to_data)+'\n'
+            if os.name == 'nt':
+                with Path(save_path, 'run_%s.ps1' % index).open('w') as f_out:
+                    f_out.write(commend)
+            else:
+                with Path(save_path, 'run_%s.run' % index).open('w') as f_out:
+                    f_out.write(commend)
