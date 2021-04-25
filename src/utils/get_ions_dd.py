4d98ace9313b1f58b30e29e210e2e632d3453bef
Bartek J <barjas02@gmail.com>
2021-04-25 13:48:17 +0200

    Fix contact flags

--- src/utils/get_ions_dd.py
+++ src/utils/get_ions_dd.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 from src.GenerateXYZ import GenerateXYZ
@@ -9,7 +8,7 @@ from src.utils.config import get_config
 
 
 if __name__ == "__main__":
-    sim_path = Path("F:\\KMC_data\\data_2021_03_24_v0\\25_11_11_random_0_a_0_1.0")
+    sim_path = Path("F:\\KMC_data\\data_2021_04_17_v0\\45_9_9_random_0_a_0_1.0_sw_500")
     data_path = sim_path / 'oxygen_map\\positions.xyz'
 
     config = get_config(sim_path / 'input.kmc')
