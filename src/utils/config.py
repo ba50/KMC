849290f8ba03dce205c5a9f82b8fdef2dcb332fe
Bartłomiej Jasik <bartlomiejjasik@macbook-air-panoramix.home>
2021-05-02 17:52:50 +0200

    Add ions dd on last point in time

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,7 +5,7 @@ def get_config(path: Path):
     str_index = [0]
     float_index = [*range(1, 8)]
     bool_index = [9, 10]
-    float_index.extend([*range(11, 16)])
+    float_index.extend([*range(11, 18)])
     with path.open() as _config:
         lines = _config.readlines()
     _config = {}
--- src/utils/get_ions_dd.py
+++ src/utils/get_ions_dd.py
@@ -1,14 +1,17 @@
 import argparse
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from src.GenerateXYZ import GenerateXYZ
+from src.utils.config import get_config
 
 
-def main(args):
-    data_path = args.data_path / 'oxygen_map' / 'positions.xyz'
+def get_ions_dd_last_points(data_path):
+    conf = get_config(data_path / 'input.kmc')
+    data_path = data_path / 'oxygen_map' / 'positions.xyz'
 
     num_atoms, raw_frames = GenerateXYZ.read_frames_dataframe(data_path)
 
@@ -27,18 +30,30 @@ def main(args):
             ions_dd['y'].append(ions_count/num_atoms)
     ions_dd = pd.DataFrame(ions_dd)
 
-    for time_step in time_steps[[0, 100, 199]]:
-        plt.plot(x_positions, ions_dd[ions_dd['time'] == time_step]['y'], label=f'{time_step} [ps]')
+    last_points = []
+    for time_step in time_steps:
+        smooth_y = ions_dd[ions_dd['time'] == time_step]['y']
+        smooth_y = smooth_y.rolling(14).sum()
+        last_points.append(smooth_y.iloc[-2])
 
-    plt.xlabel('x')
-    plt.ylabel('Ions density')
+    plt.plot(time_steps, last_points, label=f"{conf['energy_base']}")
+
+def main(args):
+    sim_path_list = args.data_path.glob('*')
+    sim_path_list = [i for i in sim_path_list if i.is_dir()]
+    for sim_path in sim_path_list:
+        get_ions_dd_last_points(sim_path)
+
+    plt.xlabel('time_step')
+    plt.ylabel('Ions density last point')
     plt.legend()
-    plt.show()
+    plt.savefig(args.data_path / 'ions_dd_last_points.png')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, required=True, help="path to simulation data")
+    parser.add_argument("--smooth", type=int, default=14, help="smoothing factor")
 
     main_args = parser.parse_args()
 
